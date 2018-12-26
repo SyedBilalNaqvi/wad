6885d47305399829c13552b8a78c34936c251524
SyedBilalNaqvi <45290996+SyedBilalNaqvi@users.noreply.github.com>
2018-12-26 18:01:21 +0500

    practice done

---
+++ syed.php
@@ -0,0 +1,18 @@
+<p>
+PRACTICE
+</p>
+
+<?php
+$tree=array("TREES COMBAT CLIMATE CHANGE","TREES PROVIDE A CANOPY AND HABITAT FOR WILDLIFE","You can improve the efficiency of your heat pump by shading it with a tree.","Trees create an ecosystem to provide habitat and food for birds and other animals.","Trees create microclimates suitable for growing shade loving plants.","Trees can help offset the buildup of carbon dioxide in the air and reduce the  greenhouse effect.Trees can create lasting impression on how a community is perceived by visitors and affect the mood and community pride of its residents.","The feeling of community pride created by trees can help reduce crime.","By reducing heating and cooling cost, trees can reduce our dependance on oil and natural gas.","By absorbing and deflecting falling rain, trees can reduce the severity of floods.","By absorbing and deflecting falling rain, trees can reduce the severity of floods.");
+
+for($i=0;$i<10;$i++)
+{
+echo "<li>";
+echo $tree[$i];
+echo "</li>";
+}
+?>
+
+
+
+
